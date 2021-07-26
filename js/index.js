c3706edd1fa0f906025d3d349aa88b689237b89a
VLRNT <dicardianw@gmail.com>
2021-07-26 14:51:36 +0300

    add carousel imgs

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// TAB
 document.querySelectorAll('.tab-trigger').forEach((item) =>
     item.addEventListener('click', function (e) {
         e.preventDefault();
@@ -13,3 +14,37 @@ document.querySelectorAll('.tab-trigger').forEach((item) =>
     }) 
 );
 document.querySelector('.tab-trigger').click();
+// Company carousel
+const slides = [
+    'img/lavazza.png',
+    'img/folgers.png',
+    'img/frontier.png',
+    'img/melitta.png',
+    'img/greenmountain.png',
+    'img/monarch.png',
+    'img/costa.png'
+]
+let currentSlide = [0, 1, 2, 3, 4];
+function showCurrentSlide() {
+    const slideTarget = document.querySelector('.current-slide');
+    // slideTarget.classList.add('border');
+    slideTarget.src = slides[currentSlide];
+}
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= slides.length) currentSlide = 0;
+    showCurrentSlide();
+}
+
+function prevSlide() {
+    currentSlide--;
+    if (currentSlide < 0) currentSlide = slides.length - 1;
+    showCurrentSlide();
+}
+
+document.querySelector('.next').addEventListener('click', nextSlide);
+document.querySelector('.prev').addEventListener('click', prevSlide);
+
+showCurrentSlide();
+setInterval(nextSlide, 3000);
